74d2109eaf7e0beab55f8a3f79a7e473ebffb225
Benjamin R. Haskell <benizi@benizi.com>
2008-10-25 05:32:47 -0400

    Allow "pattern" input. (any non-digits will be filled in)

--- bin/luhn.mod10.check.pl
+++ bin/luhn.mod10.check.pl
@@ -13,10 +13,11 @@ sub valid {
 }
 while (<>) {
 	chomp;
-	my $l = 16 - length;
-	my $add = 0;
-	while ($add < 10 ** $l) {
-		my $whole = sprintf "$_%0${l}d", $add++;
+	my $l = 16 - tr/0-9/0-9/;
+	for my $n (0..10**$l-1) {
+		my $insert = sprintf "%0*d", $l, $n;
+		my $whole = "$_";
+		$whole =~ s/\D/$_/ for split //, $insert;
 		print "$whole -> valid\n" if valid($whole);
 	}
 #	print "$_ -> ", valid($_) ? "valid" : "invalid", $/;
