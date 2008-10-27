948c66f8025237209fb15769ddb7db818119600a
Benjamin R. Haskell <benizi@benizi.com>
2008-10-27 06:12:48 -0400

    Find top-N biggest files in a given filesystem.

---
+++ bin/top-N.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Find;
+use Cwd;
+use Getopt::Long qw/:config pass_through/;
+my $N = 23;
+GetOptions(
+	'N=i' => \$N,
+) or die 'options';
+my @dirs;
+@ARGV = grep { not
+	-d() ? (push @dirs, $_) :
+0 } @ARGV;
+die "Bad options: @ARGV\n" if @ARGV;
+@dirs = (cwd) if !@dirs;
+
+my @F;
+my %exclude;
+$exclude{$_}++ for
+	map glob($_),
+	map { ("/home/bhaskell/$_", "/L/home/bhaskell/$_") }
+	qw{.mozilla mail_fu massive_mail_cleanup ripping sda 1[0-9]-*};
+my $c = 0;
+sub commafy {
+	local $_ = shift;
+	1 while s/(?<=\d)(?=\d\d\d\b)/,/;
+	$_
+}
+find {
+	wanted => sub {
+		@F = grep { -f $$_[1] } @F if !($c++ % 10000);
+		return unless -f;
+		return unless @F < $N or $F[0][0] < -s;
+		@F = sort { $$a[0] <=> $$b[0] or $$a[1] cmp $$b[1] } @F, [ -s, $File::Find::dir."/$_" ]; 
+		shift @F if @F > $N;
+		print $_+1, ": ",commafy($F[$_][0]),"\t$F[$_][1]\n" for 0..$#F;
+	},
+	preprocess => sub {
+		my %b;
+		$b{$_}++ for @_;
+		my @r = grep !$exclude{$File::Find::dir."/$_"}, @_;
+		my %a;
+		$a{$_}++ for @r;
+		warn "Skipped $_\n" for grep !$a{$_}, keys %b;
+		@r;
+	},
+}, @dirs;
