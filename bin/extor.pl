3f7fb4d8842f5ecc2f10eaf510fd0e65e6e216ce
Benjamin R. Haskell <benizi@benizi.com>
2009-01-15 11:38:00 -0500

    Allow link extraction from more elements.

--- bin/extor.pl
+++ bin/extor.pl
@@ -4,13 +4,23 @@ use warnings;
 use Getopt::Long qw/:config pass_through/;
 GetOptions(
 	'base=s' => \(my $B),
+	'att|attr=s' => \(my $attr = 'href'),
+	'gi|tag=s' => \(my $gi = 'a'),
 ) or die 'options';
 @ARGV = grep { not
 	/^http:/ ? ($B ? (1) : ($B = $_)) :
 0 } @ARGV;
 
 my $p;
-BEGIN { $p = "HTML::LinkExtor"; }
+BEGIN { $p = "HTML::Parser"; }
 eval "use $p;";
+use URI::URL;
 
-$p->new(sub{if("a"eq shift){print for{@_}->{href}}},$B)->parse_file($_)for@ARGV;
+$p->new(start_h => [
+	sub{
+		my ($this_gi, $att) = @_;
+		return unless lc $this_gi eq lc $gi;
+		return unless exists $$att{$attr};
+		print url($$att{$attr}, $B)->abs
+	}, "tag, attr"]
+)->parse_file($_)for@ARGV;
