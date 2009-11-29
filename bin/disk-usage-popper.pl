c9a6df881c0362c4bcdee15f818718fdf0916e18
Benjamin R. Haskell <bhaskell@hp.internal>
2009-11-29 00:34:26 -0500

    Better configuration, cross-host handling.

--- bin/disk-usage-popper.pl
+++ bin/disk-usage-popper.pl
@@ -7,8 +7,10 @@ use Statfs;
 use Getopt::Long;
 GetOptions(
 	'delay=i' => \(my $delay_start = 0),
-	'wm' => \(my $no_wm = !length(`pgrep ion3`)),
+	'wm!' => \(my $no_wm),
+	'justbat' => \(my $just_battery = 0),
 ) or die 'options';
+$no_wm //= !length `pgrep ion3`;
 sleep $delay_start;
 my $mw = MainWindow->new(-title=>'diskusage');
 {
@@ -39,26 +41,63 @@ my $label = $mw->Label(
 	-justify=>right=>
 	-textvariable=>\$label_text
 )->pack;
+my @temps;
+my $temp_path;
+my $processors;
+my $batteries;
+my @warnings;
 {
-	my $last_checked = 0;
-	my @to_check = qw{/ /usr:U /home:H};
-	my $prefs = "$ENV{HOME}/.duprc";
+	my @to_check;
+	my @prefs = (map "$ENV{HOME}/$_", qw/.duprc .dupmessage/);
+	sub pref_info {
+		my $info = {};
+		$$info{$_} = (!-f) ? '0:0' : join ':', 1, (stat)[9] for @prefs;
+		$info;
+	}
+	my $last = {};
+	$$last{$_} = '0:0' for @prefs;
 	sub update_prefs {
-		if (-f $prefs and (stat $prefs)[8] > $last_checked) {
-			local @ARGV = $prefs;
-			chomp(@to_check = <>);
-			$last_checked = time;
+		my $current = pref_info;
+		my @updates = grep $$current{$_} ne $$last{$_}, keys %$last;
+		$last = $current;
+		if (@updates) {
+			local @ARGV = grep $$current{$_} ne '0:0', keys %$current;
+			@to_check = ();
+			@warnings = ();
+			if (@ARGV) { while (<>) {
+				chomp;
+				my $eq = qr/\s*=\s*/;
+				if (s/^temps$eq//) {
+					my ($low, $mid, $path) = split;
+					$path //= "/proc/acpi/thermal_zone/*/temperature";
+					$temp_path = $path;
+					@temps = ($low, $mid);
+				} elsif (s/^procs$eq//) {
+					$processors = $_;
+				} elsif (s/^batts$eq//) {
+					$batteries = $_;
+				} elsif (s/^message(?:$eq)?//) {
+					push @warnings, $_;
+				} elsif (s/^font$eq//) {
+					$label->configure(-font => $_);
+				} else {
+					push @to_check, $_;
+				}
+			} }
 		}
+		@to_check = qw{/ /usr:U /home:H} unless @to_check;
 		@to_check;
 	}
 }
 sub update {
+	update_prefs;
 	my @parts;
-	my @warn;
+	my @warn = @warnings;
 	local $_ = '';
-	for (update_prefs) {
+	for (grep !$just_battery, update_prefs) {
 		my $split = qr/[:\t]/;
-		my ($dir, $disp) = /$split/ ? (split /$split/) : ($_) x 2;
+		my ($dir, $disp, $limit) = /$split/ ? (split /$split/) : ($_) x 2;
+		$limit ||= 0;
 		my (@devinos) = map { (stat)[0,1] } $dir, "$dir/..";
 		next if $devinos[0] == $devinos[2] and $devinos[1] != $devinos[3];
 		$_ .= "" for $dir,$disp;
@@ -68,7 +107,7 @@ sub update {
 		push @parts, sprintf "%2s\0%d\0%d%s",
 			$disp, 100 * (1 - ($avail/$tot)), $avail,
 			(0?sprintf("\n\0%02d", $tot):'');
-next if $dir eq '/home' and $avail > 200;#FIXME
+		next if $limit and $avail > $limit;
 		push @warn, "LOW DISK SPACE!!\n'\U$dir\E' is low!\n($avail M left)"
 			if
 				($avail < 100 and $avail/$tot < .05) or
@@ -86,8 +125,8 @@ next if $dir eq '/home' and $avail > 200;#FIXME
 		}
 		$_ = join "\n", @lines;
 	}
-	if(0){#new laptop
-		local @ARGV = (glob '/proc/acpi/battery/BAT0/{info,state}');
+	if ($batteries) {
+		local @ARGV = (glob $batteries);
 		my $cap = 1;
 		my $curr = 0;
 		while (<>) {
@@ -99,20 +138,37 @@ next if $dir eq '/home' and $avail > 200;#FIXME
 		push @parts, sprintf "Bat: %03d", $pct*100;
 		push @warn, "BATTERY $pct" if $pct < .2;
 	}
-	if(0){#new laptop
-		local @ARGV = ('/proc/acpi/thermal_zone/THRM/temperature');
+	if ($temp_path) {
+		local @ARGV = (glob $temp_path);
+		my ($low, $mid) = @temps;
 		while (<>) {
 			chomp;
 			next unless s/^temperature:\s*//;
 			my $c = /(\d+)/ ? $1 : 0;
 			my $f = ($c * 1.8) + 32;
 			push @parts, sprintf "%02dC / %03dF", $c, $f;
-			if ($c >= 65) {
+			if ($c >= $mid) {
 				push @warn, "HOT HOT HOT";
-			} elsif ($c >= 60) {
+			} elsif ($c >= $low) {
 				push @parts, "heating up";
 			}
 		}
+	}
+	if ($processors) {
+		my @cpus = (glob $processors);
+		my $count = 0 + @cpus;
+		# push @parts, "$count CPUS";
+		# local @ARGV = (grep -f, glob "$cpu/sysdev/cpufreq/cpuinfo_cur_freq");
+		# @ARGV or last;
+		# chomp(my @freq = <>);
+		# push @parts, "FREQ(@freq)";
+		# local @ARGV = (glob "$cpu/thermal_cooling/cur_state");
+		# chomp(my @throt = <>);
+		# $_ /= 1e6 for @freq;
+		# push @parts, sprintf "CPU%01d %3.2fT%d", $_, $freq[$_], $throt[$_] for 0..$#freq;
+	}
+	{
+		push @warn, map "\U$_\E IS UP", grep { -f "/tmp/$_-is-up" } qw/elation wordnet/;
 	}
 	
 	$label_text = join "\n",
