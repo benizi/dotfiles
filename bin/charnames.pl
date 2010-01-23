20b8658653ff488be3f4161081a995c62e987abb
Benjamin R. Haskell <benizi@benizi.com>
2010-01-23 16:45:57 -0500

    Much better options parsing, and generation mode.

--- bin/charnames.pl
+++ bin/charnames.pl
@@ -22,31 +22,43 @@ sub unimp { &warnonce; undef }
 UNICODY
 }
 use Unicode::UCD 'charinfo';
-my $nonlatin = 0;
-my $nonascii = 0;
-my $uniq = 0;
-@ARGV = grep { not
-	/^-*non(lat(?:in)?)?$/i ? ($nonlatin=1) :
-	/^-*lat(?:in)?$/i ? ($nonlatin=1) :
-	/^-*ascii$/i ? ($nonascii=1) :
-	/^-*u(?:niq(?:ue)?)?$/i ? (++$uniq) :
-0 } @ARGV;
+use Getopt::Long qw/:config pass_through/;
+GetOptions(
+	'latin|nonlatin' => \(my $nonlatin = 0),
+	'ascii|nonascii' => \(my $nonascii = 0),
+	'unique+' => \(my $uniq = 0),
+	'chr|low=s' => \(my $low = 0),
+	'high=s' => \(my $high = 0),
+	'all' => \(my $show_all = 0),
+) or die 'options';
+($low, $high) = ($1, $2) if ($low||'') =~ /^([\da-fx]+)(?:\.\.|-)([\da-fx]+)$/i;
+$_ = oct for $low, $high;
+$low ||= 0x20 if $high;
+$high ||= 0x4000 if $low;
 my $han = Unicode::Unihan->new;
+
+sub info {
+	local $_ = @_ ? shift : $_;
+	my $c = $_;
+	$show_all or $c =~ s/([[:^print:]])/sprintf "\\u%04x", ord $1/ge;
+	my $x = sprintf "%04x", ord;
+	my $uni = charinfo(ord);
+	my $n = charnames::viacode(ord);
+	my $script = $$uni{script} || $$uni{block} || '';
+	next if $nonascii and $c =~ /[\x20-\x7e]/;
+	next if $nonlatin and $script =~ /^(?:common|latin)$/i;
+	my $h = ($script eq 'Han') ? $han->Mandarin($_) : '';
+	print join "\t", $c, $x, $n||$h||'(no info)';
+	print "\n";
+}
+
+if ($low) {
+	info for map chr, $low..$high;
+	exit;
+}
 my %seen;
 while (<>) {
 	chomp;
 	%seen = () unless $uniq > 1;
-	for (split //) {
-		(my $c = $_) =~ s/([[:^print:]])/sprintf "\\u%04x", ord $1/ge;
-		my $x = sprintf "%04x", ord;
-		my $uni = charinfo(ord);
-		my $n = charnames::viacode(ord);
-		my $script = $$uni{script} || $$uni{block} || '';
-		next if $nonascii and $c =~ /[\x20-\x7e]/;
-		next if $nonlatin and $script =~ /^(?:common|latin)$/i;
-		my $h = ($script eq 'Han') ? $han->Mandarin($_) : '';
-		next if $uniq and $seen{$_}++;
-		print join "\t", $c, $x, $n||$h||'(no info)';
-		print "\n";
-	}
+	info for grep !$uniq || !$seen{$_}++, split //;
 }
