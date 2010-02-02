c0acc3852ad0e123f1eea7e3a1f65970eae213fe
Benjamin R. Haskell <benizi@benizi.com>
2010-02-02 09:43:27 -0500

    Added initial-list and better options.

--- bin/primes.pl
+++ bin/primes.pl
@@ -6,29 +6,42 @@ $|=1;
 # Pick the first prime p := 2
 # Pick the p'th prime (2nd = 3). Repeat
 
-my @p = (2,3,5,7,11,13,17,19);
+use Getopt::Long;
+GetOptions(
+	'nprimes=i' => \(my $n_primes = 0),
+	'max=i' => \(my $max_prime = 0),
+	'readp=s' => \(my $initial_list = ''),
+) or die 'options';
+die "Unknown option(s) (@ARGV)\n" if @ARGV;
+
+my @p;
+if ($initial_list) {
+	local @ARGV = ($initial_list);
+	chomp(@p = <>);
+} else {
+	@p = (2,3,5,7,11,13,17,19);
+}
+@p = @p[0..$n_primes-1] if $n_primes and $n_primes < @p;
+@p = grep $_<=$max_prime, @p if $max_prime;
+
 print "$_\n" for @p;
 my @c = ();
 my $l = $p[-1];
-my $p = 1;
-while (1) {
-	while (@p < $p) {
-		if (!@c) {
-			@c = grep $_ % 5, grep $_ % 3, map $l + $_, map 2 * $_, 1..30;
-			$l = $c[-1];
-		}
-		cand: while (my $c = shift @c) {
-			my $csq = 1+int sqrt $c;
-			divisor: for my $d (@p) {
-				last divisor if $d > $csq;
-				next divisor if $c % $d;
-				next cand;
-			}
-			push @p, $c;
-			print "$c\n";
-		}
+prime: while (1) {
+	if (!@c) {
+		@c = grep $_ % 5, grep $_ % 3, map $l + $_, map 2 * $_, 1..30;
+		$l = $c[-1];
 	}
-	$p++;
-	#$p = $p[$p-1];
-	#print $p, (@p < 20) ? " (@p)" : "", "\n";
+	cand: while (my $c = shift @c) {
+		last prime if $max_prime and $c > $max_prime;
+		my $csq = 1+int sqrt $c;
+		divisor: for my $d (@p) {
+			last divisor if $d > $csq;
+			next divisor if $c % $d;
+			next cand;
+		}
+		push @p, $c;
+		print "$c\n";
+		last prime if $n_primes and @p >= $n_primes;
+	}
 }
