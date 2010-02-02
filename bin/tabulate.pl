edee90e17b8ec7ebc34e9e5cbe7fbad329779223
Benjamin R. Haskell <benizi@benizi.com>
2010-02-02 09:36:46 -0500

    (to rebase w/ 1 above) Better headers support, better color support, default 'length'

--- bin/tabulate.pl
+++ bin/tabulate.pl
@@ -3,6 +3,7 @@ use open ':utf8', ':std';
 use strict;
 use warnings;
 use List::Util qw/min max/;
+use CGI qw/:standard *tbody/;
 use Getopt::Long qw/:config pass_through/;
 GetOptions(
 	'maxlength=i' => \(my $maxl),
@@ -13,6 +14,7 @@ GetOptions(
 	'insep=s' => \(my $insep = "\t"),
 	'trim!' => \(my $trim_spaces = 1),
 	'header' => \(my $has_hdr = 0),
+	'hdr=s' => \(my $headers = ''),
 	'repeatheader|r' => \(my $rpt_head = 0),
 	'html' => \(my $html = 0),
 	'texty' => \(my $texty = 0),
@@ -21,9 +23,12 @@ GetOptions(
 	'stylefile=s' => \(my $stylefile = ''),
 	'notable' => \(my $notable = 0),
 	'markodd' => \(my $mark_odd = 0),
-	'escape!' => \(my $escape = 1),
 	'sort' => \(my $do_sort = 0),
 ) or die 'options';
+my @headers = split /,/, $headers;
+@headers and $has_hdr = 1;
+$use_max = 1 if defined $maxl;
+$maxl //= 20 if $use_max;
 $texty = 1 if !$html;
 my $class = $classname ? qq{ class="$classname"} : '';
 my $td = qq{<td$class>};
@@ -45,6 +50,7 @@ $style
 HEAD
 }
 
+sub llength { local $_=@_?shift:$_; s{\e\[[\d;]+m}{}g; length }
 $sep =~ s/([\$\`])/\\$1/g if $texty;
 $sep = eval "qq\x00$sep\x00" if $sep =~ /[\\\$]/;
 if (!length $sep) {
@@ -59,24 +65,35 @@ chomp(my @l = <>);
 @l = sort @l if $do_sort;
 $insep = qr/$insep/ if $insep and $insep =~ /\\/;
 $insep = ' ' if $insep eq "\t" and !grep /\t/, @l[0..min 10, $#l];
-@l = map [map { ($texty and $use_max and length()>$maxl)?substr($_,0,$maxl):$_ } split$insep], @l;
+@l = map [map { ($texty and $use_max and llength()>$maxl)?substr($_,0,$maxl):$_ } split$insep], @l;
 my $maxe = max map 0+@$_, @l;
 @$_ < $maxe and push @$_, ('') x ($maxe - @$_) for @l;
 my @len;
-push @len, min grep($use_max, $maxl), max map length($l[$_][@len]), 0..$#l for 0..$#{$l[0]};
+push @len, min grep($use_max, $maxl), max map llength($l[$_][@len]), 0..$#l for 0..$#{$l[0]};
 my $form = join $sep, map "%-${_}s", @len;
+@headers and splice @l, 0, 0, [@headers];
 if ($texty) {
 	for (@l) {
 		$_ = sprintf $form, @$_;
 		s/\s+$// if $trim_spaces;
 		$_ .= "\n";
 	}
+	$l[0] = "\e[7m".uncolor($l[0])."\e[0m" if $color and $has_hdr;
 } else {
-	if ($escape) { for (@l) { s/&/&amp;/g, s/>/&gt;/g, s/</&lt;/g for @$_; } }
-	$l[$_] = (((1+$_) % 2)?$tr_odd:"<tr>").
-		("$td".join($sep, @{$l[$_]}).'</td></tr>'.$/) for 0..$#l;
+	for (0..$#l) {
+		my $func = ($has_hdr and !$_) ? \&th : \&td;
+		my $atts = {};
+		$$atts{class} = 'odd' if $mark_odd and $_ % 2;
+		$l[$_] = Tr(join '', map $func->($atts, $_), @{$l[$_]});
+		if ($has_hdr and !$_) {
+			$l[$_] = thead($l[$_]);
+		} else {
+			$l[$_] = start_tbody.$l[$_] if $_ == 1-($has_hdr ? 0 : 1);
+			$l[$_] = $l[$_].end_tbody if $_ == $#l;
+		}
+		$l[$_] .= "\n";
+	}
 }
-$l[0] = "\e[7m".uncolor($l[0])."\e[0m" if $color and $has_hdr;
 sub uncolor { local $_ = shift; s/\e\[[\d;]+m//g; $_ }
 if ($has_hdr and $rpt_head) {
 	my $head = $l[0];
