c1c7bb5a0c15ee9a7cdcba05050050098eaaea84
Benjamin R. Haskell <benizi@benizi.com>
2010-06-30 01:27:07 -0400

    Fixed line spacing from USC files

--- bin/colorizediff-stdin.pl
+++ bin/colorizediff-stdin.pl
@@ -3,8 +3,8 @@ use strict;
 use warnings;
 use Getopt::Long;
 GetOptions(
-   'files' => \(my $create_files = 0),
-   'diffcmd=s' => \(my $diff_cmd = 'kdiff3 %1$s %2$s'),
+	'files' => \(my $create_files = 0),
+	'diffcmd=s' => \(my $diff_cmd = 'kdiff3 %1$s %2$s'),
 ) or die 'options';
 my $need_hunk = 0;
 my @AB = \my (@A, @B);
@@ -12,61 +12,61 @@ my @ind;
 my @in;
 sub dollar { my ($str, $n) = @_; substr $str, $-[$n], $+[$n]-$-[$n] }
 while (<>) {
-   my $orig = $_;
-   if (/^\\ No newline at end of file/ and @in == 1) {
-      my $lines = $in[0][-1];
-      $lines // die "No newline at end of what file?\n";
-      $$lines[-1] =~ s/(?=[\r\n]*$)/<no newline>/;
-   } elsif (/^Files (.*?) and (.*?) differ/) {
-      push @{$AB[$_]},
-         [{ filename => dollar $orig, $_+1 }],
-         [ 'Binary files differed' ]
-         for 0..$#AB;
-   } elsif (/^Only in (.*?): (.*?)$/) {
-      push @{$AB[$_]},
-         [{ filename => $1.'/'.$2.'('.$_.')' }],
-         [ 'Path only in one or the other' ]
-         for 0..$#AB
-   } elsif ($need_hunk) {
-      @in = (
-         s/^ // ? (\@A,\@B) :
-         s/^-// ? (\@A) :
-         s/^\+// ? (\@B) :
-         die "Expecting line of hunk, but got: $orig\n"
-      );
-      my $nolead = $_;
-      push @{$$_[-1]}, $nolead for @in;
-      $need_hunk-- for @in;
-   } elsif (/^\@\@ -(\d+),(\d+) \+(\d+),(\d+) \@\@/) {
-      $need_hunk = $2 + $4;
-      if (@ind) {
-         @ind == 2 or die "Didn't find proper indexes around line $.\n";
-         push @$_, [{ filename => shift @ind }] for @AB;
-      }
-      push @$_, [] for @AB;
-   } else {
-   # if (/^diff\s(?:-\S+\s)*(\S+) (\S+)/) {
-   # push @ind, $1, $2;
-   # } els
-      if (/^--- (\S+)/) {
-         @ind >= 2 or unshift @ind, $1;
-      } elsif (/^\+\+\+ (\S+)/) {
-         @ind >= 2 or push @ind, $1;
-      } else {
-         # header etc.
-         push @$_, [{ header => $orig }] for \@A, \@B;
-      }
-   }
+	my $orig = $_;
+	if (/^\\ No newline at end of file/ and @in == 1) {
+		my $lines = $in[0][-1];
+		$lines // die "No newline at end of what file?\n";
+		$$lines[-1] =~ s/(?=[\r\n]*$)/<no newline>/;
+	} elsif (/^Files (.*?) and (.*?) differ/) {
+		push @{$AB[$_]},
+		[{ filename => dollar $orig, $_+1 }],
+		[ 'Binary files differed' ]
+		for 0..$#AB;
+	} elsif (/^Only in (.*?): (.*?)$/) {
+		push @{$AB[$_]},
+		[{ filename => $1.'/'.$2.'('.$_.')' }],
+		[ 'Path only in one or the other' ]
+		for 0..$#AB
+	} elsif ($need_hunk) {
+		@in = (
+			s/^ // ? (\@A,\@B) :
+			s/^-// ? (\@A) :
+			s/^\+// ? (\@B) :
+			die "Expecting line of hunk, but got: $orig\n"
+		);
+		my $nolead = $_;
+		push @{$$_[-1]}, $nolead for @in;
+		$need_hunk-- for @in;
+	} elsif (/^\@\@ -(\d+),(\d+) \+(\d+),(\d+) \@\@/) {
+		$need_hunk = $2 + $4;
+		if (@ind) {
+			@ind == 2 or die "Didn't find proper indexes around line $.\n";
+			push @$_, [{ filename => shift @ind }] for @AB;
+		}
+		push @$_, [] for @AB;
+	} else {
+		# if (/^diff\s(?:-\S+\s)*(\S+) (\S+)/) {
+		# push @ind, $1, $2;
+		# } els
+		if (/^--- (\S+)/) {
+			@ind >= 2 or unshift @ind, $1;
+		} elsif (/^\+\+\+ (\S+)/) {
+			@ind >= 2 or push @ind, $1;
+		} else {
+			# header etc.
+			push @$_, [{ header => $orig }] for \@A, \@B;
+		}
+	}
 }
 for my $arr (@AB) {
-   for my $i (reverse 0..$#$arr-1) {
-      my ($this, $next) = @$arr[$i,$i+1];
-      next if grep !@$_, $this, $next;
-      next if grep !ref($$_[0]), $this, $next;
-      next if grep !exists($$_{header}), map $$_[0], $this, $next;
-      $$this[0]{header} .= $$next[0]{header};
-      splice @$arr, $i+1, 1;
-   }
+	for my $i (reverse 0..$#$arr-1) {
+		my ($this, $next) = @$arr[$i,$i+1];
+		next if grep !@$_, $this, $next;
+		next if grep !ref($$_[0]), $this, $next;
+		next if grep !exists($$_{header}), map $$_[0], $this, $next;
+		$$this[0]{header} .= $$next[0]{header};
+		splice @$arr, $i+1, 1;
+	}
 }
 use ColorDiff;
 #ColorDiff::Format('html');
--- bin/gitfuse.pl
+++ bin/gitfuse.pl
@@ -4,19 +4,19 @@ use warnings;
 use bytes;
 use Getopt::Long;
 GetOptions(
-   'mountpoint=s' => \$::mount_point,
-   'debug+' => \($::debug = 0),
-   'branchslash=s' => \($::slash = '%'),
-   'gitdir=s' => \$::gitdir,
-   '<>' => sub {
-      if (!defined $::mount_point) {
-         $::mount_point = shift;
-      } elsif (!defined $::gitdir) {
-         $::gitdir = shift;
-      } else {
-         die "Already specified --mountpoint and --gitdir\n";
-      }
-   },
+	'mountpoint=s' => \$::mount_point,
+	'debug+' => \($::debug = 0),
+	'branchslash=s' => \($::slash = '%'),
+	'gitdir=s' => \$::gitdir,
+	'<>' => sub {
+		if (!defined $::mount_point) {
+			$::mount_point = shift;
+		} elsif (!defined $::gitdir) {
+			$::gitdir = shift;
+		} else {
+			die "Already specified --mountpoint and --gitdir\n";
+		}
+	},
 ) or die 'options';
 defined $::mount_point or die "Need to specify --mountpoint\n";
 defined $::gitdir or die "Need to specify --gitdir\n";
@@ -41,43 +41,43 @@ use POSIX qw/:errno_h ENOENT/;
 sub _de { $::debug or return; warn "", (caller(1))[3], "(@_)\n"; }
 sub _branches { my @br = $git->command('branch'); s{^..}{} and s{/}{$::slash}g for @br; @br }
 sub _parts {
-   my $fn = shift;
-   my @parts = split m{/}, $fn, -1;
-   shift @parts;
-   my $branch = shift @parts;
-   my $dir = join '/', @parts;
-   ($fn, $branch, $dir)
+	my $fn = shift;
+	my @parts = split m{/}, $fn, -1;
+	shift @parts;
+	my $branch = shift @parts;
+	my $dir = join '/', @parts;
+	($fn, $branch, $dir)
 }
 sub _lsfile {
-   my ($fn, $branch, $dir) = _parts shift;
-   my @got;
-   for ($git->command('ls-tree', '--long', $branch, '--', $dir)) {
-      my ($ptss, $name) = split /\t/;
-      if ($name =~ s/^"//) {
-         my $orig = qq{"$name};
-         $name =~ s/"$//;
-         my $newname = '';
-         while (length $name) {
-            if ($name =~ s/^(\\[nrt]|\\[0-7]{3})//) {
-               $newname .= eval qq{qq.$1.};
-            } elsif ($name =~ s/^\\(")// || $name =~ s/^(.)//) {
-               $newname .= $1;
-            } else {
-               #print "Couldn't parse: ", Dumper([$orig,$newname,$name]);
-               last;
-            }
-         }
-         $name = $newname;
-         #length $name or print Dumper [name=>join '', map /\w/?$_:sprintf("%%%02x",ord), split //, join '^', $name,$orig];
-      }
-      my ($perm, $type, $sha1, @size) = split ' ', $ptss;
-      my $size = pop @size;
-      $size =~ /\D/ and $size = 0;
-      $size = 0 + $size;
-      $name =~ s{^\Q$dir\E}{};
-      push @got, { perm => oct "0$perm", type => $type, sha1 => $sha1, size => $size, name => $name };
-   }
-   @got;
+	my ($fn, $branch, $dir) = _parts shift;
+	my @got;
+	for ($git->command('ls-tree', '--long', $branch, '--', $dir)) {
+		my ($ptss, $name) = split /\t/;
+		if ($name =~ s/^"//) {
+			my $orig = qq{"$name};
+			$name =~ s/"$//;
+			my $newname = '';
+			while (length $name) {
+				if ($name =~ s/^(\\[nrt]|\\[0-7]{3})//) {
+					$newname .= eval qq{qq.$1.};
+				} elsif ($name =~ s/^\\(")// || $name =~ s/^(.)//) {
+					$newname .= $1;
+				} else {
+					#print "Couldn't parse: ", Dumper([$orig,$newname,$name]);
+					last;
+				}
+			}
+			$name = $newname;
+			#length $name or print Dumper [name=>join '', map /\w/?$_:sprintf("%%%02x",ord), split //, join '^', $name,$orig];
+		}
+		my ($perm, $type, $sha1, @size) = split ' ', $ptss;
+		my $size = pop @size;
+		$size =~ /\D/ and $size = 0;
+		$size = 0 + $size;
+		$name =~ s{^\Q$dir\E}{};
+		push @got, { perm => oct "0$perm", type => $type, sha1 => $sha1, size => $size, name => $name };
+	}
+	@got;
 }
 =begin STAT fields
 00 dev      device number of filesystem
@@ -95,76 +95,76 @@ sub _lsfile {
 12 blocks   actual number of blocks allocated
 =cut
 sub _fakestat {
-   my ($info) = @_;
-   my $perm = $$info{perm};
-   $perm |= 0444 unless $perm & 0444;
-   my $size = $$info{size} // 0;
-   my $links = 1; # TODO
-   if ($perm & 040000) {
-      $perm |= 0111;
-      $size ||= 4096;
-      $links++;
-   } elsif (($perm & 0120000) == 0120000) {
-      $perm |= 0777;
-   }
-   1234, 1234, $perm, $links, 0+$<, 0+$(, 0, $size, time, time, time, 4096, 0;
+	my ($info) = @_;
+	my $perm = $$info{perm};
+	$perm |= 0444 unless $perm & 0444;
+	my $size = $$info{size} // 0;
+	my $links = 1; # TODO
+	if ($perm & 040000) {
+		$perm |= 0111;
+		$size ||= 4096;
+		$links++;
+	} elsif (($perm & 0120000) == 0120000) {
+		$perm |= 0777;
+	}
+	1234, 1234, $perm, $links, 0+$<, 0+$(, 0, $size, time, time, time, 4096, 0;
 }
 my %special = map {; $_ => 1 } qw/.index .working/;
 sub _rev {
-   my ($rev, $subst) = @_;
-   $subst and $rev =~ s/$::slash/\//g;
-   return $rev if $special{$rev};
-   my @out;
-   eval {
-      @out = $git->command(['rev-parse','-q','--verify',$rev],{STDERR=>0});
-   };
-   return _rev($rev, 1) if !@out and $rev =~ /$::slash/;
-   return unless @out;
-   shift @out;
+	my ($rev, $subst) = @_;
+	$subst and $rev =~ s/$::slash/\//g;
+	return $rev if $special{$rev};
+	my @out;
+	eval {
+		@out = $git->command(['rev-parse','-q','--verify',$rev],{STDERR=>0});
+	};
+	return _rev($rev, 1) if !@out and $rev =~ /$::slash/;
+	return unless @out;
+	shift @out;
 }
 sub getattr {
-   &_de;
-   my $fn = shift;
-   my @ret = (-&ENOENT);
-   if ($fn eq '/') {
-      @ret = _fakestat {perm => 16384}
-   } elsif ($fn =~ m{^/([^/]+)$}) {
-      my $rev = $1;
-      @ret = _fakestat {perm => 16384} if _rev $rev;
-   } else {
-      if (my ($info) = _lsfile $fn) {
-         @ret = _fakestat $info;
-         if ($::debug > 1) {
-            print Dumper [$fn, $info, \@ret];
-         }
-      }
-   }
-   @ret;
+	&_de;
+	my $fn = shift;
+	my @ret = (-&ENOENT);
+	if ($fn eq '/') {
+		@ret = _fakestat {perm => 16384}
+	} elsif ($fn =~ m{^/([^/]+)$}) {
+		my $rev = $1;
+		@ret = _fakestat {perm => 16384} if _rev $rev;
+	} else {
+		if (my ($info) = _lsfile $fn) {
+			@ret = _fakestat $info;
+			if ($::debug > 1) {
+				print Dumper [$fn, $info, \@ret];
+			}
+		}
+	}
+	@ret;
 }
 sub getdir {
-   &_de;
-   my $fn = shift;
-   my @files = (qw/. ../);
-   if ($fn eq '/') {
-      push @files, HEAD => (keys %special), _branches;
-   } else {
-      push @files, map $$_{name}, _lsfile "$fn/";
-      $::debug and print Dumper [_lsfile "$fn/"], [@files];
-   }
-   @files, 0;
+	&_de;
+	my $fn = shift;
+	my @files = (qw/. ../);
+	if ($fn eq '/') {
+		push @files, HEAD => (keys %special), _branches;
+	} else {
+		push @files, map $$_{name}, _lsfile "$fn/";
+		$::debug and print Dumper [_lsfile "$fn/"], [@files];
+	}
+	@files, 0;
 }
 sub unlink { &_de; $_[0] eq '/unmount' and exit; -&EOPNOTSUPP }
 sub readlink {
-   &_de;
-   my ($fn, $branch, $dir) = _parts shift;
-   my ($info) = _lsfile $fn;
-   $info or return '';
-   $$info{perm} & 0120000 or return '';
-   $git->command('cat-file', '-p', $$info{sha1});
+	&_de;
+	my ($fn, $branch, $dir) = _parts shift;
+	my ($info) = _lsfile $fn;
+	$info or return '';
+	$$info{perm} & 0120000 or return '';
+	$git->command('cat-file', '-p', $$info{sha1});
 }
 our $AUTOLOAD;
 sub AUTOLOAD {
-   (my $_sub = $AUTOLOAD) =~ s{^.*::}{};
-   eval "sub $_sub { &_de; -&EOPNOTSUPP }";
-   goto &$_sub;
+	(my $_sub = $AUTOLOAD) =~ s{^.*::}{};
+	eval "sub $_sub { &_de; -&EOPNOTSUPP }";
+	goto &$_sub;
 }
