ee9a4029f765d0533e84cdfac3120a7acc868e94
Benjamin R. Haskell <benizi@benizi.com>
2011-03-29 11:00:02 -0400

    bitblock: Unicode print thing, modded x-kbstate

--- bin/src/X11/x-kbstate.c
+++ bin/src/X11/x-kbstate.c
@@ -3,15 +3,26 @@
 #include <X11/keysym.h>
 #include <X11/extensions/XKBstr.h>
 #include <stdio.h>
+#include "bitblock.h"
 
-int main(void) {
+int main(int argc, char **argv) {
+	int i, use_unicode = 0;
 	Display *disp = XOpenDisplay(NULL);
 	XkbStateRec kbstate;
-	int i;
+	for (i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-u")) {
+			use_unicode = 1;
+		}
+	}
 	if (!disp) return 1;
 	if (!XkbGetState(disp, XkbUseCoreKbd, &kbstate)) {
-		for (i=7; i>=0; i--)
-			printf("%d", ((kbstate.mods >> i) & 1));
+		if (use_unicode) printf("│");
+		if (use_unicode) {
+			printbyteblock(kbstate.mods);
+		} else
+			for (i=7; i>=0; i--)
+				printf("%d", ((kbstate.mods >> i) & 1));
+		if (use_unicode) printf("│");
 		printf("\n");
 	} else {
 		printf("Request failed.\n");
