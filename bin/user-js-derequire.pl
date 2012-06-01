7f30057f1d4c93f7a171cade83f184f806d20854
Benjamin R. Haskell <benizi@benizi.com>
2012-06-01 03:24:49 -0400

    derequire: add [src/] [dst/] mode

--- bin/user-js-derequire.pl
+++ bin/user-js-derequire.pl
@@ -28,25 +28,52 @@ sub get {
    <$f>
 }
 
-my (@header, @rest);
-while (<>) {
-   chomp;
-   if (/==UserScript==/../==\/UserScript==/) {
-      push @header, $_;
-   } else {
-      push @rest, $_;
+sub derequire_file {
+   local @ARGV = @_;
+   my (@header, @rest);
+   while (<>) {
+      chomp;
+      if (/==UserScript==/../==\/UserScript==/) {
+         push @header, $_;
+      } else {
+         push @rest, $_;
+      }
    }
+   my @url;
+   @header = grep {!(
+      /\@require\s+(\S+)/ ? (push @url, $1)
+   : 0)} @header;
+   chomp(my @req = map {
+      ("/* {{{1 required $_ */",
+      get($_),
+      "/* }}}1 required $_ */")
+   } @url);
+   print "$_\n" for @header, @req, @rest
 }
-my @url;
-@header = grep {!(
-   /\@require\s+(\S+)/ ? (push @url, $1)
-: 0)} @header;
-chomp(my @req = map {
-   ("/* {{{1 required $_ */",
-   get($_),
-   "/* }}}1 required $_ */")
-} @url);
-print "$_\n" for @header, @req, @rest
+
+if (@ARGV == 2) {
+     my ($src, $dest) = @ARGV;
+     if (-d $src and (!-e $dest or -d $dest)) {
+        chdir $src or die "Couldn't chdir($src): $!";
+        for my $file (glob '*.user.js') {
+           my $mtime = (stat $file)[9];
+           my $dfile = "$dest/$file";
+           if (-f $dfile and $mtime < (stat $dfile)[9]) {
+              print "[$file] Derequired file is newer.\n";
+              next;
+           }
+           print "[$file] Derequiring to [$dfile].\n";
+           open my $f, '>', $dfile or die "Couldn't output to $dfile: $!";
+           my $stdout = select $f;
+           derequire_file $file;
+           select $stdout;
+        }
+        exit;
+     }
+}
+
+derequire_file $_ for @ARGV;
+
 __END__
 #original:
 perl -lnwe '/==UserScript==/../==\/UserScript==/?push@header,$_:push@rest,$_; END { @header = grep {!( /\@require\s+(\S+)/ ? (push @url, $1) : 0 )} @header; chomp(@req = map { ("/* {{{1 required $_ */", readpipe("GET $_"), "/* }}}1 required $_ */") } @url); print for @header,@req,@rest; }' ~/.mozilla/firefox/default/gm_scripts/eosutil/eosutil.user.js > ~/without-require/eosutil.user.js
