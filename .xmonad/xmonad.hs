5f65375ec51b9fcc998b0491a3b7703d6104989e
Benjamin R. Haskell <xmonad@benizi.com>
2012-10-01 10:18:02 -0400

    Try using "tiled" as default layout

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -211,7 +211,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = avoidStruts $ Full ||| tiled ||| Mirror tiled
+myLayout = avoidStruts $ tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
