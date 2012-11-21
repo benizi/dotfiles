57890f270ccc24d5352192358bc69940f2071f8b
Benjamin R. Haskell <emacs@benizi.com>
2012-11-21 01:07:55 -0500

    Add melpa for latest nrepl-ritz

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,7 +22,8 @@
 (require 'package)
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages/")))
+      ("marmalade" . "http://marmalade-repo.org/packages/")
+      ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 ;; Set color-theme if running in X
