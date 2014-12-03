4d027753d9a8e5047bb07d3b0b87a4446c956ab8
Benjamin R. Haskell <dotfiles@benizi.com>
2014-12-02 21:18:42 -0500

    Load `datomic.clj` from `.lein/profiles.clj`
    - but only if it exists
    
    I'm not sure this is what I'll do long-term, but I don't like the
    alternative (GPG-encrypted credentials), because it still doesn't seem
    to solve the "Don't add the Datomic repo to every project" problem.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,7 @@
-{:user {:plugins [[lein-try "0.4.1"]
-                  [lein-pprint "1.1.1"]
-                  [cider/cider-nrepl "0.8.0"]]}}
+{:user #=(merge
+           {:plugins [[lein-try "0.4.1"]
+                      [lein-pprint "1.1.1"]
+                      [cider/cider-nrepl "0.8.0"]]}
+           #=(eval (try (read-string (slurp (str (System/getProperty "user.home")
+                                                 "/.config/datomic.clj")))
+                     (catch Exception e {}))))}
