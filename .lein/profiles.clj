9a1cfd257b85078cab751ea43b6a48fb77ac00ee
Benjamin R. Haskell <dotfiles@benizi.com>
2015-09-30 01:47:33 -0400

    Bump to latest `cider/cider-nrepl`

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user #=(merge
           {:plugins [[lein-try "0.4.1"]
                      [lein-pprint "1.1.1"]
-                     [cider/cider-nrepl "0.9.0-SNAPSHOT"]]}
+                     [cider/cider-nrepl "0.10.0-SNAPSHOT"]]}
           #=(eval (try (read-string (slurp (str (System/getProperty "user.home")
                                                 "/.config/datomic.clj")))
                        (catch Exception e {}))))}
