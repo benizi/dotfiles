8b7fe567476f8a94ab1d041a5143a3d15f58d571
Benjamin R. Haskell <dotfiles@benizi.com>
2016-01-13 01:48:24 -0500

    Add my `lein-datomic-pro` plugin to my lein config
    Still kinda WIP, but it should work for now.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user #=(merge
           {:plugins [[lein-try "0.4.1"]
                      [lein-pprint "1.1.1"]
+                     [lein-datomic-pro "0.0.1-SNAPSHOT"]
                      [cider/cider-nrepl "0.10.0-SNAPSHOT"]]}
           #=(eval (try (read-string (slurp (str (System/getProperty "user.home")
                                                 "/.config/datomic.clj")))
