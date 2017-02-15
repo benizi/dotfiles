6af3bd46a8705c4d2e4c786fa9fab9b064c1cf3a
Benjamin R. Haskell <dotfiles@benizi.com>
2017-02-15 00:43:31 -0500

    Add `def` implementations in CompatibilityShims
    Turns out this was a difference between `xmonad` versions (0.11 vs
    0.12), not GHC versions. So, solved it by installing newer XMonad.  I'm
    going to immediately revert this, but wanted it for posterity.

--- .xmonad/lib/CompatibilityShims.hs
+++ .xmonad/lib/CompatibilityShims.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE CPP #-}
+#if __GLASGOW_HASKELL__ < 710
+{-# LANGUAGE TypeFamilies #-}
+#endif
 
 module CompatibilityShims (
 #if __GLASGOW_HASKELL__ < 710
+    def,
     (<$>)
 #endif
 )
@@ -9,4 +13,22 @@ where
 
 #if __GLASGOW_HASKELL__ < 710
 import Data.Functor ((<$>))
+
+-- Needed for defaults:
+import XMonad (XConfig)
+import XMonad.Actions.GridSelect (HasColorizer(..), GSConfig(..), defaultGSConfig)
+import XMonad.Config (defaultConfig)
+import XMonad.Hooks.DynamicLog (PP(..), defaultPP)
+import XMonad.Layout (Choose, Full, Mirror, Tall)
+import XMonad.Layout.Decoration (Theme(..), defaultTheme)
+import XMonad.Prompt (XPConfig(..), defaultXPConfig)
+
+class Default a where
+  def :: a
+instance Default XPConfig where def = defaultXPConfig
+instance (HasColorizer a) => Default (GSConfig a) where def = defaultGSConfig
+instance Default Theme where def = defaultTheme
+instance Default PP where def = defaultPP
+instance (a ~ Choose Tall (Choose (Mirror Tall) Full)) => Default (XConfig a)
+  where def = defaultConfig
 #endif
