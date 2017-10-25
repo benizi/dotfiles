aaaf43b1cb903b054af22de24d7d56be92b41c1e
Benjamin R. Haskell <dotfiles@benizi.com>
2017-10-25 12:10:45 -0400

    Add ways to specify "MemAvailable" in `force-swap`
    Previously, it was useful to find and parse the `MemAvailable:` line
    from `/proc/meminfo`:
    
        ## Allocate all "available" memory, then 500MiB more
        $ awk '/MemAvail/ {print $2 "K 500M"}' /proc/meminfo |
          xargs force-swap
    
    Now, that doesn't require parsing with `awk`:
    
        ## Allocate all "available" memory, then 500MiB more
        $ force-swap all 500M
    
        ## synonyms:
        $ force-swap avail 500M
        $ force-swap available 500M
        $ force-swap --avail 500M
        $ force-swap --available 500M

--- bin/src/force-swap.cc
+++ bin/src/force-swap.cc
@@ -6,10 +6,13 @@
  * - Use C++ style instead of C.
  * - Parse cli arguments instead of interactive prompting
  * - Don't assume values are MiB (bytes unless trailing 'K', 'M', or 'G')
+ * - Adds some synonyms for "all available memory": "all", "avail", "--avail".
  */
+#include <fstream>
 #include <iostream>
 #include <limits>
 #include <new>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -19,6 +22,31 @@ using namespace std;
 
 #define FAIL(msg) { cout << msg << endl; exit(EXIT_FAILURE); }
 
+// Constants.
+static const vector<string> kAvailableFlags = {"all", "avail", "--avail"};
+static const string kProcMeminfo("/proc/meminfo");
+static const string kMemAvailable("MemAvailable:");
+
+// Function declarations.
+static size_t available_memory();
+static bool has_any_prefix(const string&, const vector<string>&);
+static bool has_prefix(const string&, const string&);
+static size_t parse(const string&);
+
+// Find the MemAvailable field from /proc/meminfo.
+static size_t available_memory() {
+  ifstream meminfo(kProcMeminfo);
+  string line;
+  while (getline(meminfo, line)) {
+    if (!has_prefix(line, kMemAvailable)) continue;
+    istringstream input(line.substr(kMemAvailable.size()));
+    size_t kb;
+    input >> kb;
+    return kb << 10;
+  }
+  FAIL("Couldn't find " << kMemAvailable << " line in " << kProcMeminfo);
+}
+
 // Parse a string size into an amount of memory to allocate.
 static size_t parse(const string& size) {
   size_t converted = 0;
@@ -26,6 +54,8 @@ static size_t parse(const string& size) {
   unsigned long shift = 0;
   unsigned long max_ul = numeric_limits<unsigned long>::max();
   size_t max_sz = numeric_limits<size_t>::max();
+
+  if (has_any_prefix(size, kAvailableFlags)) return available_memory();
 
   // Parse the size to an unsigned int.
   try {
@@ -51,6 +81,20 @@ static size_t parse(const string& size) {
   val <<= shift;
   if (val > max_sz) FAIL("Too big for size_t: " << val);
   return val;
+}
+
+// Returns true if haystack has needle as a prefix.
+static bool has_prefix(const string& haystack, const string& needle) {
+  return equal(needle.begin(), needle.end(), haystack.begin());
+}
+
+// Returns true if haystack has any of the needles as a prefix.
+static bool has_any_prefix(const string &haystack,
+                           const vector<string> &needles) {
+  for (const string& needle : needles) {
+    if (has_prefix(haystack, needle)) return true;
+  }
+  return false;
 }
 
 }  // namespace
