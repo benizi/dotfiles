322a2a764191fc1ccabde1ad385ee1e36329072a
Benjamin R. Haskell <dotfiles@benizi.com>
2018-01-12 16:18:45 -0500

    Run prettify against `convertquotes.js`

--- bin/convertquotes.js
+++ bin/convertquotes.js
@@ -11,19 +11,19 @@
 //     long text
 //     `,
 //
-let LineBreaker = require('linebreak');
+let LineBreaker = require("linebreak");
 
 const aslines = sentence => {
   let breaker = new LineBreaker(sentence),
     ret = [],
-    line = '',
+    line = "",
     last = 0,
     bk;
-  while (bk = breaker.nextBreak()) {
+  while ((bk = breaker.nextBreak())) {
     let token = sentence.slice(last, bk.position);
     if (80 < (line + token).length || bk.required) {
-      ret.push(line.replace(/\s*$/,''));
-      line = '';
+      ret.push(line.replace(/\s*$/, ""));
+      line = "";
     }
     line += token;
     last = bk.position;
@@ -34,23 +34,27 @@ const aslines = sentence => {
 
 {
   let fh = process.stdin,
-    line = '';
-  fh.setEncoding('utf8');
-  fh.on('readable', () => {
-    let chunk = '';
-    while (chunk = fh.read()) line += chunk;
+    line = "";
+  fh.setEncoding("utf8");
+  fh.on("readable", () => {
+    let chunk = "";
+    while ((chunk = fh.read())) line += chunk;
   });
-  fh.on('end', () => {
+  fh.on("end", () => {
     let start = line.indexOf(`"`),
       end = line.length - line.split("").reverse().join("").indexOf(`"`),
-      key = line.substr(0,start),
-      quoted = line.substr(start,end-start),
+      key = line.substr(0, start),
+      quoted = line.substr(start, end - start),
       //_ = console.log({start,end,key,quoted,line}),
       trail = line.substr(end),
       raw = eval(quoted),
-      lines = raw.split(/\.  (?=[A-Z])/).map(l=>l.replace(/\.*$/,'.')).map(aslines).join(""),
+      lines = raw
+        .split(/\.  (?=[A-Z])/)
+        .map(l => l.replace(/\.*$/, "."))
+        .map(aslines)
+        .join(""),
       subbed = "\n" + lines.replace(/`/g, "\\`"),
-      out = [key,subbed,trail].join("`");
+      out = [key, subbed, trail].join("`");
     process.stdout.write(out);
   });
 }
