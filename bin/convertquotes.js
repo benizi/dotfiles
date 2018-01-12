d202416fa93cae08fd889ced1ae15c2594d63ddf
Benjamin R. Haskell <dotfiles@benizi.com>
2018-01-12 16:21:28 -0500

    Extract `wrapLine` function
    Better than writing it as an inline callback function.

--- bin/convertquotes.js
+++ bin/convertquotes.js
@@ -33,28 +33,28 @@ const aslines = sentence => {
 };
 
 {
-  let fh = process.stdin,
+  let wrapLine = line => {
+      let start = line.indexOf(`"`),
+        end = line.length - line.split("").reverse().join("").indexOf(`"`),
+        key = line.substr(0, start),
+        quoted = line.substr(start, end - start),
+        trail = line.substr(end),
+        raw = eval(quoted),
+        lines = raw
+          .split(/\.  (?=[A-Z])/)
+          .map(l => l.replace(/\.*$/, "."))
+          .map(aslines)
+          .join(""),
+        subbed = "\n" + lines.replace(/`/g, "\\`"),
+        out = [key, subbed, trail].join("`");
+      process.stdout.write(out);
+    },
+    fh = process.stdin,
     line = "";
   fh.setEncoding("utf8");
   fh.on("readable", () => {
     let chunk = "";
     while ((chunk = fh.read())) line += chunk;
   });
-  fh.on("end", () => {
-    let start = line.indexOf(`"`),
-      end = line.length - line.split("").reverse().join("").indexOf(`"`),
-      key = line.substr(0, start),
-      quoted = line.substr(start, end - start),
-      //_ = console.log({start,end,key,quoted,line}),
-      trail = line.substr(end),
-      raw = eval(quoted),
-      lines = raw
-        .split(/\.  (?=[A-Z])/)
-        .map(l => l.replace(/\.*$/, "."))
-        .map(aslines)
-        .join(""),
-      subbed = "\n" + lines.replace(/`/g, "\\`"),
-      out = [key, subbed, trail].join("`");
-    process.stdout.write(out);
-  });
+  fh.on("end", () => wrapLine(line));
 }
