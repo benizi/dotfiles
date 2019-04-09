01b29669cc571b8fed5d0f75c06b28e4deaded97
Benjamin R. Haskell <dotfiles@benizi.com>
2019-04-09 12:15:15 -0400

    Add Emacs Magit color customizations
    Makes Magit's diffs much less garish. (No more pastel green and red.)
    Probably relies too much on background colors, but it feels fine w/ dark
    backgrounds.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,3 +7,17 @@
    (quote
     (go-mode magit yari yaml-mode volatile-highlights smooth-scrolling smex smartparens scss-mode sass-mode rainbow-mode projectile paredit monroe mic-paren markdown-mode inf-ruby haskell-mode gnu-apl-mode git-gutter expand-region evil elscreen color-theme-railscasts cider auto-complete anything)))
  '(xterm-extra-capabilities (quote modifyOtherKeys)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-diff-added ((t (:background "#030" :foreground "#fff"))))
+ '(magit-diff-added-highlight ((t (:background "#060" :foreground "#fff"))))
+ '(magit-diff-context ((t (:background "#003" :foreground "#999"))))
+ '(magit-diff-context-highlight ((t (:background "#006" :foreground "#fff"))))
+ '(magit-diff-hunk-heading ((t (:background "#336" :foreground "#fff"))))
+ '(magit-diff-hunk-heading-highlight ((t (:background "#66f" :foreground "#fff" :weight bold))))
+ '(magit-diff-removed ((t (:background "#300" :foreground "#fff"))))
+ '(magit-diff-removed-highlight ((t (:background "#600" :foreground "#fff"))))
+ '(magit-section-highlight ((t (:background "#c60" :foreground "#fff")))))
