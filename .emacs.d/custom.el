fd1da47d13eb171687709148d0022b3cb60c643d
Benjamin R. Haskell <dotfiles@benizi.com>
2019-04-09 12:12:28 -0400

    Add Emacs `Custom` selected packages
    This subsumes a lot of what `.emacs.d/lisp/early-packages.el` was trying
    to do. I'm not sure I like using `Custom` for this, but it captures the
    current state of my main laptop, so pushing it in order to share config.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,4 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-mode magit yari yaml-mode volatile-highlights smooth-scrolling smex smartparens scss-mode sass-mode rainbow-mode projectile paredit monroe mic-paren markdown-mode inf-ruby haskell-mode gnu-apl-mode git-gutter expand-region evil elscreen color-theme-railscasts cider auto-complete anything)))
  '(xterm-extra-capabilities (quote modifyOtherKeys)))
