b096a37670182fa933ff2d2e374621281a9b3b29
Benjamin R. Haskell <dotfiles@benizi.com>
2019-06-03 00:24:16 -0400

    Use modern JS in `console-downloader` bookmarklet
    "modern" might mean "less ancient" here.

--- bookmarklets/console-downloader.js
+++ bookmarklets/console-downloader.js
@@ -1,33 +1,23 @@
 // from: http://bgrins.github.io/devtools-snippets/#console-save
 // via: https://stackoverflow.com/a/19818659/82723
 {
-  let saver = (data, filename = "console.json") => {
+  let saveimg = (url, name = null) => {
+    let href = new URL(url),
+      download = name === null ? href.pathname.split("/").pop() : name,
+      e = new MouseEvent("click", { bubbles: true, view: window }),
+      props = { href, download };
+    Object.assign(document.createElement("a"), props).dispatchEvent(e);
+  };
+  let save = (data, filename = "console.json") => {
     let out = JSON.stringify(data, null, 1),
       type = "application/json",
       blob = new Blob([out], { type }),
-      e = document.createEvent("MouseEvents"),
+      e = new MouseEvent("click", { bubbles: true, view: window }),
       a = document.createElement("a");
     a.download = filename;
     a.href = window.URL.createObjectURL(blob);
     a.dataset.downloadurl = [type, a.download, a.href].join(":");
-    e.initMouseEvent(
-      "click",
-      true,
-      false,
-      window,
-      0,
-      0,
-      0,
-      0,
-      0,
-      false,
-      false,
-      false,
-      false,
-      0,
-      null
-    );
     a.dispatchEvent(e);
   };
-  saver({ a: 1, b: 2 });
+  Object.assign(console, { save, saveimg });
 }
